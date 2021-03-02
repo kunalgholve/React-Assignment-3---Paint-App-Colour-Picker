2f5155c30d2d046ccc1a1d517dae337a8e69c917
kunal <gkunal7996@gmail.com>
2021-03-02 15:46:40 +0530

    done paint

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,13 @@
+import React, { useState } from 'react';
+import '../styles/Child.css';
+const Selection=({applyColor} )=>{
 
+    const[bg,setbg]=useState({ background: "" });
+    return(
+        <div className="fix-box" style={bg} onClick={()=>applyColor(setbg)}>
+                        <h2 className="subheading">Selection</h2>
+        </div>
+    );
+};
+
+export default Selection;
